eaaa779da5f06cd1141b328397e3a570a71312ae
DMSchool <DMSchool@users.noreply.github.com>
2019-09-18 14:36:45 -0700

    Ghost Wall

---
+++ Assets/Scripts/KeyCollection.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeyCollection : MonoBehaviour
+{
+
+    public Transform player;
+    public GameObject GohstWall;
+
+    void OnTriggerEnter(Collider col)
+    {
+        Debug.Log("bang");
+        if (col.gameObject.name == player.name)
+        {
+            Debug.Log("hit");
+            Destroy(GohstWall);
+            Destroy(this.gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/KeyMovement.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeyMovement : MonoBehaviour
+{
+
+    void Update()
+    {
+        transform.Rotate(Vector3.up, 50 * Time.deltaTime);
+    }
+}
